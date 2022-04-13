0037a78e094003ac49de8f0eb4b8b8613cccde22
Eid Hachem <eidhachem1@gmail.com>
2022-04-13 19:47:53 +0300

    Add function to generate instructors section from js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,78 @@
 const hamburger = document.querySelector('.ham');
+const navbar = document.querySelector('.navbar');
 
 function openCloseMenu() {
   hamburger.classList.toggle('change');
+  navbar.classList.toggle('.open-navbar');
 }
 
 hamburger.addEventListener('click', openCloseMenu);
+
+const speakers = [
+  {
+    image: '../css/images/instructors/b1.jpg',
+    imageAlt: 'Picture of Ben Parker',
+    name: 'Ben Parker',
+    position: 'Design Director at University of London',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque labore ex ad impedit, mollitia rerum et non',
+  },
+  {
+    image: '../css/images/instructors/g1.jpg',
+    imageAlt: 'Picture of zoe Fernandez',
+    name: 'Zoe Fernandez',
+    position: 'Web Application Developer at Facebook',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque labore ex ad impedit, mollitia rerum et non',
+  },
+  {
+    image: '../css/images/instructors/g2.jpg',
+    imageAlt: 'Picture of Edward Cisneros',
+    name: 'Edward Cisneros',
+    position: 'SEO Specialist at Google',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque labore ex ad impedit, mollitia rerum et non',
+  },
+  {
+    image: '../css/images/instructors/b2.jpg',
+    imageAlt: 'Picture of May Gauthier',
+    name: 'May Gauthier',
+    position: 'Interaction Designer at Lenovo',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque labore ex ad impedit, mollitia rerum et non',
+  },
+  {
+    image: '../css/images/instructors/b3.jpg',
+    imageAlt: 'Picture of Vince Veras',
+    name: 'Vince Veras',
+    position: 'Visual Designer at Microverse',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque labore ex ad impedit, mollitia rerum et non',
+  },
+  {
+    image: '../css/images/instructors/g3.jpg',
+    imageAlt: 'Picture of Chris Powel',
+    name: 'Chris Powel',
+    position: 'JavaScript Engineer at Apple',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque labore ex ad impedit, mollitia rerum et non',
+  },
+];
+
+const speakerSection = document.querySelector('.instructors');
+if (speakerSection) {
+  speakerSection.innerHTML = speakers
+    .map(
+      (speaker) => `
+      <article class="instructor">
+      <img src="${speaker.image}" alt="${speaker.imageAlt}" />
+      <div>
+        <h5>${speaker.name}</h5>
+        <p class="position">${speaker.position}</p>
+        <hr />
+        <p>${speaker.description}</p>
+      </div>
+    </article>`
+    )
+    .join('');
+}
